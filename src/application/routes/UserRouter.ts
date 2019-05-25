4f75180c0149a35cb46e382d87fc693c00289b05
Miguel Angel Hernandez Cruz <mhernandez@unphu.edu.do>
2019-05-25 16:57:14 -0400

    creation of utility to validate the parameters received by the request

--- src/application/routes/UserRouter.ts
+++ src/application/routes/UserRouter.ts
@@ -2,8 +2,9 @@ import BaseRouter from './base/BaseRouter';
 import ResponseHandler from '../util/ResponseHandler';
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import UserController from '../../presentation/controllers/UserController';
-import { httpCodes } from '../config/ErrorCode';
+import { buildError, httpCodes } from '../config/ErrorCode';
 import GetUserLoginQuery from '../../domain/queries/GetUserLoginQuery';
+import PayloadValidator from '../util/PayloadValidator';
 
 export default class UserRouter extends BaseRouter {
   constructor(route: string, protected userController: UserController) {
@@ -13,6 +14,20 @@ export default class UserRouter extends BaseRouter {
 
   addRoutes(): void {
     this.router.get('/:username', this.show());
+  }
+
+  create(): RequestHandler {
+    return(req: Request, res: Response) => {
+      const payloadValidate = new PayloadValidator(req);
+      payloadValidate.validate(['username',
+        'password', 'person', 'role', 'userStatus']);
+      const errors = payloadValidate.getErrors();
+
+      if (errors) {
+        const responseError = buildError(httpCodes.BAD_REQUEST, errors);
+        return ResponseHandler.sendError(res, responseError);
+      }
+    };
   }
 
   show(): RequestHandler {
