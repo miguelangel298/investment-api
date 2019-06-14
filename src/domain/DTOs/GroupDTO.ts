f4cefd5fde0c2bb82c72a6771569cda2f74278d9
Miguel Angel Hernandez Cruz <mhernandez@unphu.edu.do>
2019-06-13 22:38:36 -0400

    DTO for mapping of data for the  response HTTP to client

--- src/domain/DTOs/GroupDTO.ts
+++ src/domain/DTOs/GroupDTO.ts
@@ -1,6 +1,3 @@
-import { Column, CreateDateColumn, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import UserEntity from '../../data/entities/UserEntity';
-import PermissionGroupEntity from '../../data/entities/PermissionGroupEntity';
 import GroupEntity from '../../data/entities/GroupEntity';
 import PermissionGroupDTO from './PermissionGroupDTO';
 import UserDTO from './UserDTO';
