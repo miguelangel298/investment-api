c8a12b1113786046fd3416d8a68a5f997fea20e0
Miguel Angel Hernandez Cruz <mhernandez@unphu.edu.do>
2019-06-22 11:51:03 -0400

    Query for get genders list

--- src/data/repository/GenderRepository/IGenderRepository.ts
+++ src/data/repository/GenderRepository/IGenderRepository.ts
@@ -1,8 +1,11 @@
-import { FindOneOptions, ObjectID } from 'typeorm';
+import { FindConditions, FindOneOptions, ObjectID } from 'typeorm';
 import GenderEntity from '../../entities/GenderEntity';
 
 export default interface IGenderRepository {
 
   findOne(id?: string | number | Date | ObjectID, options?:
     FindOneOptions<GenderEntity>): Promise<GenderEntity | undefined>;
+
+  find(conditions?: FindConditions<GenderEntity>): Promise<GenderEntity[]>;
+
 }
